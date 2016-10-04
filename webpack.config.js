80a4cd11ed99718f2d571c677cd69938f6d03459
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-10-04 16:28:23 +0200

    Added webpack dep and config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+/* global __dirname */
+
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    library: 'ScrollSync'
+  },
+  module: {
+    loaders: [
+      {
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    // Can require('file') instead of require('file.js') etc.
+    extensions: ['', '.js', '.json']
+  },
+  externals: {
+    react: 'react'
+  }
+}
